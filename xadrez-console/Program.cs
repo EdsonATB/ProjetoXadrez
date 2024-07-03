39a91d22ea16335b01d4d33685e720b3da58828c
Edson Augusto <edsonaugusto2006@gmail.com>
2024-07-03 17:41:26 -0300

    Criado Elementos do Tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,10 +4,7 @@ namespace xadrez_console{
     class Program {
         static void Main(string[] args) {
 
-            Posicao p = new Posicao(3,4);
-
-
-            Console.WriteLine("Posiçao: " + p);
+            Tabuleiro tab = new Tabuleiro(8,8);
 
 
             Console.ReadLine();
