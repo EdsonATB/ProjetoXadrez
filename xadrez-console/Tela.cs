c8e87965e42f0489910e7448043c9580ca035a1c
Edson Augusto <edsonaugusto2006@gmail.com>
2024-07-05 18:26:14 -0300

    Comecando a colocar peças no tabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -4,7 +4,6 @@ namespace xadrez_console {
      class Tela {
 
         public static void imprimirTabuleiro(Tabuleiro tab) {
-
             for(int i = 0; i < tab.linhas; i++) {
                 for(int j = 0; j < tab.linhas; j++) {
                     if (tab.peca(i, j) == null) {
