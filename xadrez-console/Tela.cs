7a7906ff842e0161fa979b38f8f41e41bac21284
Edson Augusto <edsonaugusto2006@gmail.com>
2024-07-10 00:38:45 -0300

    Implementada logica de Xeque

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -13,6 +13,9 @@ namespace xadrez_console {
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando Jogada: " + partida.jogadorAtual);
             Console.WriteLine();
+            if (partida.xeque) {
+                Console.WriteLine("XEQUE!");
+            }
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida) {
