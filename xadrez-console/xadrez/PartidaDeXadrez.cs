b5c2610b323dcfcb57c6e923b36b9321c3f82f51
Edson Augusto <edsonaugusto2006@gmail.com>
2024-07-09 23:01:44 -0300

    Implementando restricoes de movimento

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -5,8 +5,8 @@ namespace xadrez {
     class PartidaDeXadrez {
 
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
         public bool terminada {  get; private set; }
 
         public PartidaDeXadrez() {
@@ -23,6 +23,39 @@ namespace xadrez {
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
 
+        }
+
+        public void realizaJogada(Posicao origem, Posicao destino) {
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+
+        public void validarPosicaoDeOrigem(Posicao pos) {
+            if(tab.peca(pos) == null) {
+                throw new TabuleiroException("Nao existe peça na posiçao de origem escolhida!");
+            }
+            if(jogadorAtual != tab.peca(pos).cor) {
+                throw new TabuleiroException("Nao é seu turno ainda!");
+            }
+            if(!tab.peca(pos).existeMovimentosPossiveis()) {
+                throw new TabuleiroException("Nao há movimentos possiveis para a peça de origem escolhida!");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino) {
+            if(!tab.peca(origem).podeMoverPara(destino)) {
+                throw new TabuleiroException("Posiçao de destino inválida!");
+            }
+        }
+
+        private void mudaJogador() {
+            if (jogadorAtual == Cor.Branca) {
+                jogadorAtual = Cor.Preta;
+            }
+            else {
+                jogadorAtual = Cor.Branca;
+            }
         }
 
         private void colocarPecas() {
